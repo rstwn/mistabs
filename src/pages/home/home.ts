4ce94e91ffe2a76d2d97c5526cfbb59973152084
Raul <raul.sl91@gmail.com>
2018-05-30 22:46:12 +0200

    Mi primer commit sonido

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,63 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import  { ANIMALES } from "../../data/data.animales";
+
+import { Animal } from "../../interfaces/animal.interface"
+
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  animales:Animal[] = [];
+  audio = new Audio();
+  audioTiempo: any;
+
+  constructor() {
+
+    this.animales = ANIMALES.splice(0);
 
   }
+
+  reproducir( animal:Animal ){
+
+    this.pausar_audio ( animal );
+
+    if( animal.reproduciendo ){
+      animal.reproduciendo = false;
+      return;
+    }
+
+    console.log( animal );
+
+    this.audio.src = animal.audio;
+
+    this.audio.load();
+    this.audio.play();
+
+    animal.reproduciendo = true;
+
+    this.audioTiempo = setTimeout( ()=> animal.reproduciendo = false, animal.duracion * 1000 );
+
+  }
+
+private pausar_audio( animalSel:Animal ){
+
+  clearTimeout( this.audioTiempo );
+
+  this.audio.pause();
+  this.audio.currentTime = 0;
+
+  for( let animal of this.animales){
+
+    if( animal.nombre != animalSel.nombre ){
+      animal.reproduciendo = false;
+    }
+  }
+
+}
 
 }
